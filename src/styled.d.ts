ac8506d6808c7fe345435ac897a19e30cf6ed36a
jaeuk Lee <lskkhj1005@naver.com>
2023-02-15 00:44:28 +0900

    add: 주석

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-declare module 'styled-compontnes' {
+declare module 'styled-compontnes' {    // 테마 정의
     export interface DefaultTheme {
         textColor: string;
         bgColor: string;
--- src/theme.ts
+++ src/theme.ts
@@ -2,13 +2,13 @@
 
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
+export const lightTheme: DefaultTheme = {   // type은 DefaultTheme
     bgColor: "white",
     textColor: "black",
     btnColor: "tomato",
 };
 
-export const darkTheme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {    // type은 DefaultTheme
     bgColor: "black",
     textColor: "white",
     btnColor: "teal",
