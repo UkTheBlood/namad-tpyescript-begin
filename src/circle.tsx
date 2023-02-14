97bbee7510339e555cbb19e87191ecd2cc6d80ab
jaeuk Lee <lskkhj1005@naver.com>
2023-02-14 16:08:00 +0900

    start typescript

---
+++ src/circle.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    
+`
+
+function circle() {
+  return (
+    <Container>circle</Container>
+  )
+}
+
+export default circle
