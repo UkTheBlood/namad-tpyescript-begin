a2721688fc2d987e9dcffe35f9b90d8c0a0e14d0
jaeuk Lee <lskkhj1005@naver.com>
2023-02-14 17:42:22 +0900

    learn: typescript optional props

--- src/circle.tsx
+++ src/circle.tsx
@@ -1,22 +1,41 @@
 import styled from "styled-components";
 
-interface ContainerProps {
-    bgColor: string;
-}
+// interface ContainerProps {
+//     bgColor: string;
+//     borderColor: string;
+// }
 
-const Container = styled.div<ContainerProps>`
+const Container = styled.div<CircleProps>`
     width: 200px;
     height: 200px;
-    background-color: ${props => props.bgColor};
+    background-color: ${(props) => props.bgColor};
     border-radius: 100px;
+    border: 1px solid ${(props) => props.borderColor};
 `
 
-interface CircleProp {
-    bgColor: string;
+interface CircleProps {
+    bgColor: string;        // 무조건 있어야 함
+    borderColor?: string;   // optional props => 있을 수도 있고, 없을 수도 있음
+    text?: string;
 }
 
-function Circle({bgColor}: CircleProp) {
-    return <Container bgColor={bgColor} />
+function Circle({ bgColor, borderColor, text = "default text"}: CircleProps) {
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
+    )
 }
 
 export default Circle;
+
+// interface PlayerShape {
+//     name: string;
+//     age: number;
+// }
+
+
+// const sayHello = (playerObj: PlayerShape) => `Hello ${playerObj.name} you are ${playerObj.age} years old`;
+
+// sayHello({name:"nice", age: 12})
+// sayHello({name:"hi", age: 12, hello:1})
